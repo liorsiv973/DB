a729d5c0b421e8f486c17b05f7b50b6f5e77b027
liorsiv973 <72123935+liorsiv973@users.noreply.github.com>
2021-06-27 11:19:59 +0300

    Delete ex5/zvi directory

--- ex5/zvi/create.sql
+++
@@ -1,9 +0,0 @@
-create table bestsellers(
-     Name varchar(150),
-     Author varchar(150),
-     UserRating float,
-     Reviews integer,
-     Price integer,
-     Year integer,
-     Genre varchar(150)
-);
